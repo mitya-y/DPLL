6f38f2e39ba68c34283509f332d8c6e1a6f3e610
mitya47 <8863314@gmail.com>
2024-06-23 13:01:15 +0300

    change format of programm input/output

--- main.cpp
+++ main.cpp
@@ -1,10 +1,35 @@
 #include <iostream>
 #include <ostream>
 
+#include "algorithm/dpll.hpp"
+
 #include "tests/googletest/googletest/include/gtest/gtest.h"
 
 int main(int argc, char **argv)
 {
-  ::testing::InitGoogleTest(&argc, argv);
-  return RUN_ALL_TESTS();
+  if (argc <= 1) {
+    std::cout << "fatal error: no input files\n";
+    return 47;
+  }
+
+  auto cnf = load_cnf(argv[1]);
+  if (!cnf) {
+    std::cout << "fatal error: file not existing or not satisfying DIMACS CNF format\n";
+    return 47;
+  }
+  auto answer = dpll_algorithm(cnf.value());
+
+  if (!answer) {
+    std::cout << "Not satisfaible\n";
+  }
+  else {
+    std::cout << "Satisfaible\n";
+    for (auto [variable, value] : answer.value()) {
+      std::cout << (value ? "" : "-") << variable << ' ';
+    }
+    std::cout << "\n";
+  }
+
+  // ::testing::InitGoogleTest(&argc, argv);
+  // return RUN_ALL_TESTS();
 }
